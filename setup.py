f721434ec993da3db295d14025620b3b4e101222
Josiah P <vasti009@gmail.com>
2018-05-14 20:16:30 -0300

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="TagScriptEngine",
-      version="1.0.1",
+      version="1.0.2",
       description="For advanced text interpolation",
       author="Josiah P.",
       author_email="vasti009@gmail.com",

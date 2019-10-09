cb0eab076943f830c58079ff98dba5ba88e3ea13
Josiah P <vasti009@gmail.com>
2019-10-09 18:35:47 -0300

    Added new exceptions

--- TagScriptEngine/__init__.py
+++ TagScriptEngine/__init__.py
@@ -1,2 +1,3 @@
 from .verb import Verb
+from .exceptions import WorkloadExceededError
 from .interpreter import Interpreter, build_node_tree
---
+++ TagScriptEngine/exceptions.py
@@ -0,0 +1,2 @@
+class WorkloadExceededError(Exception):
+    pass

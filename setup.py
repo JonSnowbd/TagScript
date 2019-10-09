8c8bf21cf9ebd9fada7a85323fee2d0821a0bdd8
Josiah P <vasti009@gmail.com>
2019-10-09 18:35:59 -0300

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url="https://github.com/JonSnowbd/TagScript/tree/v2",
     author="PySnow",
     author_email="vasti009@gmail.com",
-    version="2.1.1",
+    version="2.1.2",
     packages=[
         "TagScriptEngine",
         "TagScriptEngine.adapter",

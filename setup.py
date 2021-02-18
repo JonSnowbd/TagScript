bedad02336385201804db013a69cc7a110d1e15c
PhenoM4n4n <phenom4n4n@gmail.com>
2021-02-17 22:52:56 -0800

    add discord package

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     url="https://github.com/JonSnowbd/TagScript/tree/v2",
     author="PySnow",
     author_email="vasti009@gmail.com",
-    version="2.3.1",
+    version="2.3.2",
     packages=[
         "TagScriptEngine",
         "TagScriptEngine.adapter",
         "TagScriptEngine.block",
         "TagScriptEngine.interface",
+        "TagScriptEngine.discord",
     ],
 )
